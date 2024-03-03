df1e9a61fe7a4080e4634d28c53e119f5498d8ae
David Blanchard <dpjb@hotmail.fr>
2024-03-03 10:30:26 +0100

    Adds the Controller feature.

--- framework/Http/Kernel.php
+++ framework/Http/Kernel.php
@@ -2,16 +2,31 @@
 
 namespace Funbox\Framework\Http;
 
+use FastRoute\RouteCollector;
+use function FastRoute\simpleDispatcher;
+
 class Kernel
 {
 
     public function handle(Request $request): Response
     {
-        $content =  <<< HTML
-            <h1>Hello world!</h1>
-            HTML;
 
-        return new Response(content: $content);
+        $routes  = include APP_PATH . 'routes' . DIRECTORY_SEPARATOR . 'web.php';
+
+        $dispatcher = simpleDispatcher(function (RouteCollector $routeCollector) use($routes) {
+            foreach ($routes as $route) {
+                $routeCollector->addRoute(...$route);
+            }
+        });
+
+        $routeInfo = $dispatcher->dispatch(
+            $request->getMethod(),
+            $request->getPathInfo()
+        );
+
+        [$status, [$controller, $method], $vars] = $routeInfo;
+
+        return (new $controller)->$method($vars);
     }
 
 }
---
+++ framework/bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+namespace Funbox\Framework;
+
+define('BASE_PATH', dirname(__DIR__) . DIRECTORY_SEPARATOR);
+define('APP_PATH', BASE_PATH . 'app' . DIRECTORY_SEPARATOR);
+
+require_once dirname(__DIR__) . '/vendor/autoload.php';
+
