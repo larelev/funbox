54a567a019f5e1d945fc863b4ba1daacf5b6b531
David Blanchard <david.blanchard71@icloud.com>
2024-03-06 01:57:58 +0100

    Delete useless files.

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -2,19 +2,14 @@
 
 namespace App\Controllers;
 
-use App\Widgets\Widget;
 use Funbox\Framework\Http\Response;
 
 class HomeController
 {
-    public function __construct(private Widget $widget)
-    {
-
-    }
-    public function index(): Response
+    function index(): Response
     {
         return new Response(content: <<< HTML
-                    <h1>Hello {$this->widget->name}!</h1>
+                    <h1>Hello world!</h1>
                     HTML);
     }
 }
