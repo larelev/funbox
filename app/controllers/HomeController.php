84b07358ea5394d4e13f7d508229ddad80febfc2
David Blanchard <dpjb@hotmail.fr>
2024-03-09 11:55:48 +0100

    Add services management in containers.
    Adds a post input form.

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -2,25 +2,18 @@
 
 namespace App\Controllers;
 
+use App\Widgets\Widget;
 use Funbox\Framework\Http\Response;
 use Funbox\Framework\MVC\AbstractController;
 
 class HomeController extends AbstractController
 {
-<<<<<<< HEAD
-    function index(): Response
-    {
-        return new Response(content: <<< HTML
-                    <h1>Hello world!</h1>
-                    HTML);
-=======
     public function __construct(private Widget $widget)
     {
     }
 
     public function index(): Response
     {
-        return $this->render('Home.html.twig', ['name' => $this->widget->name]);
->>>>>>> 2e5f5be (Adds twig support)
+        return $this->render('home.html.twig', ['name' => $this->widget->name]);
     }
 }
