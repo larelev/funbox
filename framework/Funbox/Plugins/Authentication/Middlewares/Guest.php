a6d495af7c358b6e384ee15c4d4be28c1c43e79e
David Blanchard <dpjb@hotmail.fr>
2024-03-27 20:05:34 +0100

    Adds a guest middleware.

---
+++ framework/Funbox/Plugins/Authentication/Middlewares/Guest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Funbox\Plugins\Authentication\Middlewares;
+
+use Funbox\Framework\Http\HistoryInterface;
+use Funbox\Framework\Http\RedirectResponse;
+use Funbox\Framework\Http\Request;
+use Funbox\Framework\Http\Response;
+use Funbox\Framework\Middleware\MiddlewareInterface;
+use Funbox\Framework\Middleware\RequestHandlerInterface;
+use Funbox\Framework\Session\SessionInterface;
+
+class Guest implements MiddlewareInterface
+{
+    private bool $isAuthenticated = true;
+
+    public function __construct(
+        private readonly SessionInterface $session,
+        private readonly HistoryInterface $history,
+    )
+    {
+    }
+
+    public function process(Request $request, RequestHandlerInterface $requestHandler): Response
+    {
+        $this->session->start();
+        $this->history->set($request->getInfo());
+        $lastGetRequest = $this->history->getLastGetRequest();
+        $pathname = $lastGetRequest->getPathInfo();
+
+        if($pathname == '/login' || $pathname == '/register') {
+            $pathname = '/dashboard';
+        }
+
+        if($this->session->has(\Funbox\Plugins\Authentication\Authentication::AUTH_KEY)) {
+            return new RedirectResponse($pathname);
+        }
+
+        return $requestHandler->handle($request);
+    }
+}
--- framework/Funbox/Plugins/Authentication/Routes/Web.php
+++ framework/Funbox/Plugins/Authentication/Routes/Web.php
@@ -2,7 +2,14 @@
 
 use Funbox\Framework\Routing\Route;
 
-Route::get('/register', [\Funbox\Plugins\Authentication\Controllers\RegistrationController::class, 'index']);
+Route::get('/register', [\Funbox\Plugins\Authentication\Controllers\RegistrationController::class, 'index',
+    [
+        \Funbox\Plugins\Authentication\Middlewares\Guest::class,
+    ]]);
 Route::post('/register', [\Funbox\Plugins\Authentication\Controllers\RegistrationController::class, 'register']);
-Route::get('/login', [\Funbox\Plugins\Authentication\Controllers\LoginController::class, 'index']);
+Route::get('/login', [\Funbox\Plugins\Authentication\Controllers\LoginController::class, 'index',
+    [
+        \Funbox\Plugins\Authentication\Middlewares\Guest::class,
+    ]
+]);
 Route::post('/login', [\Funbox\Plugins\Authentication\Controllers\LoginController::class, 'login']);
