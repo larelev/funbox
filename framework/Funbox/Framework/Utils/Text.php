f36ed41f56bb0d1174a89a33f7fc0ff5d7da305b
David Blanchard <dpjb@hotmail.fr>
2024-03-30 16:46:07 +0100

    Can now make php arrays with callable.
    Laravel-like route can now manage controllers as closure!

--- framework/Funbox/Framework/Utils/Text.php
+++ framework/Funbox/Framework/Utils/Text.php
@@ -51,22 +51,108 @@ class Text
 
     public static function jsonToPhpReturnedArray(string $json): string
     {
+        $array = json_decode($json, JSON_OBJECT_AS_ARRAY);
         $result = '<?php' . PHP_EOL;
-        $result .= 'return [' . PHP_EOL;
+        $result .= 'return ';
 
-        $l = mb_strlen($json, 'UTF-8');
-        $text = mb_substr($json, 1, $l - 2);
-
-        $text = mb_ereg_replace(': ', ' => ', $text);
-        $text = mb_ereg_replace('{', '[', $text);
-        $text = mb_ereg_replace('}', ']', $text);
-        $text = mb_ereg_replace('\\\/', '/', $text);
-        $text = "\t" . trim($text);
-
-        $result .= $text . PHP_EOL;
-
-        $result .= '];' . PHP_EOL;
+        $result .= self::arrayToString($array);
+        $result .= ';' . PHP_EOL;
 
         return $result;
+    }
+
+    public static function arrayToString(array $array): string
+    {
+        $dump = self::var_dump_r($array);
+
+        $indentsLengths = [];
+        $convert = '';
+
+        $re = '/(.*)(\[[\w"-\/\\\\]+\]=>)(\n)( +)/';
+        $subst = "$1$2";
+        $entries = preg_replace($re, $subst, $dump);
+        $buffer = $entries;
+        $offset = 0;
+
+        $re = '/^( ?+)+/m';
+        preg_match_all($re, $entries, $matches, PREG_SET_ORDER, 0);
+
+        foreach ($matches as $match) {
+            $indentsLengths[] = count($match) > 1 ? strlen($match[0]) : 0;
+        }
+
+        $l = count($indentsLengths);
+        for($i = 0; $i < $l; $i++) {
+            $indentLen = $indentsLengths[$i];
+            $indent = $indentLen > 0 ? str_repeat(' ', $indentLen) : '';
+            $entryRx = '/( ?+)+(\[([\w"-\/\\\\]+)\]=>)?((array|string|int|float|bool)\(([\w.]+)\) ?(.*)\n)/';
+            $closeArrayRx = '/^( ?+)+}/';
+            
+            if (preg_match($closeArrayRx, $buffer, $matches)) {
+                $convert .= $indent . ']' . ($indent == '' ? '' : ',');
+                $convert .= PHP_EOL;
+                $stringLen = strlen($matches[0]) + 1;
+                $buffer = substr($buffer, $stringLen);
+                $offset += $stringLen;
+            } else if (preg_match($entryRx, $buffer, $matches)) {
+                $convert .= $indent;
+                if ($matches[5] == 'array') {
+                    $convert .= '[';
+                    $stringLen = strlen($matches[0]) + 1;
+                    $buffer = substr($buffer, $stringLen);
+                    $offset += $stringLen;
+                } else if ($matches[5] == 'string') {
+                    $len = intval($matches[6]);
+                    $token = "string($len)";
+                    $lenToken = strlen($token);
+                    $posToken = strpos($matches[0], $token);
+                    $start = $posToken + $lenToken + 2;
+
+                    $value = substr($entries, $offset + $start, $len);
+                    $quote = substr($value, 0, 8) == 'function' ? '' : "'";
+                    $value = $quote == '' ? $value : str_replace("\\", "\\\\", $value);
+
+                    if($j = substr_count($value, PHP_EOL)) {
+                        $i += $j;
+                    }
+                    if (str_starts_with($matches[3], '"')) {
+                        $key = "'" . trim($matches[3], '"') . "'";
+                        $key = str_replace("\\", "\\\\", $key);
+                        $convert .= $key . " => $quote" . $value . "$quote,";
+                    } else {
+                        $convert .= "$quote" . $value . "$quote,";
+                    }
+
+                    $stringLen = $start + $len + 2;
+                    $buffer = substr($buffer, $stringLen);
+                    $offset += $stringLen;
+                } else {
+                    if (str_starts_with($matches[3], '"')) {
+                        $key = "'" . trim($matches[3], '"') . "'";
+                        $key = str_replace("\\", "\\\\", $key);
+                        $convert .= $key . " => " . $matches[6] . ",";
+                    } else {
+                        $convert .= $matches[6] . ',';
+                    }
+                    $stringLen = strlen($matches[0]) + 1;
+                    $buffer = substr($buffer, $stringLen);
+                    $offset += $stringLen;
+                }
+                $convert .= PHP_EOL;
+
+            }
+        }
+
+        return $convert;
+    }
+
+    private static function var_dump_r(mixed $value): string
+    {
+        ob_start();
+        var_dump($value);
+        $content = ob_get_contents();
+        ob_end_clean();
+
+        return $content;
     }
 }
