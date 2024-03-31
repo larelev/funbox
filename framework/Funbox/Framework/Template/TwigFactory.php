d09440c2b5ddf2384fca351809d6c413392b6937
David Blanchard <dpjb@hotmail.fr>
2024-03-31 22:43:28 +0200

    TwigFactories can now be aggregated.

--- framework/Funbox/Framework/Template/TwigFactory.php
+++ framework/Funbox/Framework/Template/TwigFactory.php
@@ -2,13 +2,40 @@
 
 namespace Funbox\Framework\Template;
 
+use Funbox\Framework\Session\Session;
 use Twig\Environment;
+use Twig\TwigFunction;
 
 class TwigFactory extends AbstractTwigFactory
 {
 
-    public function extendsTemplate(Environment $twig): Environment
+    public static function extendsTemplate(Environment $twig): Environment
     {
+        $session = new Session();
+        $twig->addFunction(new TwigFunction(
+            'session',
+            function () use ($session): Session
+            {
+                return $session;
+            }
+        ));
+
+        $twig->addFunction(new TwigFunction(
+            'csrfToken',
+            function () use ($session): string
+            {
+                return $session->read(Session::CSRF_TOKEN);
+            }
+        ));
+
+        $twig->addFunction(new TwigFunction(
+            'csrfFieldName',
+            function (): string
+            {
+                return Session::CSRF_TOKEN;
+            }
+        ));
+
         return $twig;
     }
 }
