d94d959467b8b2957b5e41b547efdb4e34f0f443
David Blanchard <dpjb@hotmail.fr>
2024-04-02 01:07:58 +0200

    Refactor request objects.

--- framework/Funbox/Framework/Http/RequestInfoInterface.php
+++ framework/Funbox/Framework/Http/RequestInfoInterface.php
@@ -2,11 +2,11 @@
 
 namespace Funbox\Framework\Http;
 
-interface RequestInterface
+interface RequestInfoInterface
 {
     public function getGetParams(string $param = ''): array|string;
     public function getPostParams(string $param = ''): array|string;
-    public function getCookies(): array;
+    public function getCookies($name = ''): array|string;
     public function getFiles(): array;
     public function getServer(): array;
     public function getPathInfo(): string;
