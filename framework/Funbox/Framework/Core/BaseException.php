b52e2d14d0de4c2b683817a9c121b7f44b2fdc16
David Blanchard <dpjb@hotmail.fr>
2024-04-01 09:35:31 +0200

    Fix exception handling.

--- framework/Funbox/Framework/Core/BaseException.php
+++ framework/Funbox/Framework/Core/BaseException.php
@@ -2,12 +2,13 @@
 
 namespace Funbox\Framework\Core;
 
+use Funbox\Framework\Logger\Logger;
 use Throwable;
 
 class BaseException extends \Exception
 {
     public function __construct(string $message = "", int $code = 0, null|Throwable $previous = null, ...$params)
     {
-        parent::__construct(sprintf($message, $params), $code, $previous);
+        parent::__construct(sprintf($message, ...$params), $code, $previous);
     }
 }
